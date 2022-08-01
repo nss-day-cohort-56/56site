fe0f4eeaed9d2583ff9a9d7824e0ff417d1d9d28
Camille Faulkner <camillecfaulkner@gmail.com>
2022-08-01 18:33:47 -0500

    student list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
+// import { getStudents } from './data/cohort';
+import { cohort } from './data/cohort'
 
 export const App = () => {
-  return <>hey</>
+
+  let students = cohort
+
+  return <>
+
+    <article>
+      {
+        students.map(s => {
+          return <>
+
+            <div className="student_name">{s.name}</div>
+
+            <br />
+          </>
+        })
+      }
+    </article>
+  </>
 }
 
