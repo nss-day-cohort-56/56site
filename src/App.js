edf5dd197168713b543014c5cd07d35da62856e5
Camille Faulkner <camillecfaulkner@gmail.com>
2022-08-01 18:48:43 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
-// import { getStudents } from './data/cohort';
 import { cohort } from './data/cohort'
 
 export const App = () => {
@@ -16,7 +15,13 @@ export const App = () => {
           return <>
 
             <div className="student_name">{s.name}</div>
-
+            <div className="student_funFact">{s.funFact}</div>
+            <div className="student_email">{s.email}</div>
+            <div className="student_githubAccount">{s.githubAccount}</div>
+            <div className="student_linkedinAccount">{s.linkedinAccount}</div>
+            <div className="student_learnMoreAboutMeDescribeYourStrongSuits">{s.learnMoreAboutMeDescribeYourStrongSuits}</div>
+            <div className="student_favoriteExperienceNssRelated">{s.favoriteExperienceNssRelated}</div>
+            <div className="student_whereAreYouLocatedCityState">{s.whereAreYouLocatedCityState}</div>
             <br />
           </>
         })
