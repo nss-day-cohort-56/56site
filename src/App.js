bbb84f6dfd87d71d332fa3f938897a7f777a46c8
caleb-gore <cgore2001@gmail.com>
2022-08-01 18:54:51 -0500

    connected data.json to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 export const App = () => {
-  return <>hey</>
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('data.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  
+  useEffect(()=>{
+    getData()
+  },[])
+  
+  return (
+    <div className="App">
+     {
+       data && data.length>0 && data.map((item)=><p>{item.cohort.name}</p>)
+     }
+    
+    {
+      data?.cohort?.map(student => <p>{student.name}</p>)
+    }
+    </div>
+
+
+  );
 }
 
