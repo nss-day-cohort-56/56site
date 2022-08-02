7826d5d6d823f5000bf19f3fcf97cfc6025d3969
Camille Faulkner <camillecfaulkner@gmail.com>
2022-08-01 19:03:44 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './App.css';
 import { cohort } from './data/cohort'
 
@@ -13,12 +13,11 @@ export const App = () => {
       {
         students.map(s => {
           return <>
-
             <div className="student_name">{s.name}</div>
             <div className="student_funFact">{s.funFact}</div>
             <div className="student_email">{s.email}</div>
             <div className="student_githubAccount">{s.githubAccount}</div>
-            <div className="student_linkedinAccount">{s.linkedinAccount}</div>
+            <a href={s.linkedinAccount} target="_blank"> Linkedin Icon</a>
             <div className="student_learnMoreAboutMeDescribeYourStrongSuits">{s.learnMoreAboutMeDescribeYourStrongSuits}</div>
             <div className="student_favoriteExperienceNssRelated">{s.favoriteExperienceNssRelated}</div>
             <div className="student_whereAreYouLocatedCityState">{s.whereAreYouLocatedCityState}</div>
